82fc40c516c72c55bfe08b934790836f87a1bc0b
alexlin51 <65678216+alexlin51@users.noreply.github.com>
2020-09-09 17:47:26 -0700

    Update main.cpp

--- Linked Lists/main.cpp
+++ Linked Lists/main.cpp
@@ -3,20 +3,23 @@
 
 using namespace std;
 
-int main(){
-	
+int main() {
+
 	List test;
 
+	test.AddEnd(1);
+	test.AddEnd(2);
 	test.AddEnd(3);
 	test.AddEnd(4);
 	test.AddEnd(5);
-	test.AddEnd(6);
-	test.AddEnd(7);
-	test.AddEnd(8);
-	test.DelEnd();
-	test.DelEnd();
-	test.AddEnd(8);
-	test.InsertNode(7, 3);
+	
+	//test.DelEnd();
+	//test.InsertNode();		Edge cases tested!
+	//test.PullNode();			Edge cases tested!
+
 	test.ViewList();
+	
+	cout << " | ";
+	cout << test.DataPos(3);
 
 }
